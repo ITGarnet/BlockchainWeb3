ae13ca00477b78a49f82b0c65d9bef803342e85d
Tziki <tzgarnet@gmail.com>
2019-02-04 10:05:24 -0500

    Getting Infura to Work -2

--- deploy.js
+++ deploy.js
@@ -8,15 +8,16 @@ let mnemonic =
 // I am going to use "web3": "^1.0.0-beta.37" since the newer versions don't work.
 const provider = new HDWalletProvider(
   mnemonic,
-  "https://mainnet.infura.io/v3/3be98b330a594c559da82d2ea2fdb96e"
+  "https://mainnet.infura.io/v3/3be98b330a594c559da82d2ea2fdb96e",
+  2
 );
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  console.log("Attempting to deploy from account", accounts);
-  console.log("Attempting to deploy from account", accounts[1]);
+
+  console.log("Attempting to deploy from account", accounts[0]);
 };
 
 deploy();
