a1367b34d34697f2c8436619086235ab797ed24e
Tziki <tzgarnet@gmail.com>
2019-02-05 15:08:53 -0500

    Rendering the View

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 
 class Lottery extends Component {
   render() {
-    return <h1>Decentralized Lottery Application</h1>;
+    return (
+      <div>
+        <h1>Total lottery pool is 1000 </h1>
+        <form>
+          <input value=".5" />
+          <button type="submit">Participate</button>
+        </form>
+        <hr /> <br /> <hr />
+        <p>The manager of the lottery decentralaized app is </p>
+        <button> Pick Winner </button>
+      </div>
+    );
   }
 }
 
