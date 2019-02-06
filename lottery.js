1dce85ccc3827c3d67157289b26a4686e873b4e9
Tziki <tzgarnet@gmail.com>
2019-02-06 10:46:20 -0500

    Getting web3 installed

--- lottery.js
+++ lottery.js
@@ -1,48 +1,5 @@
 import web3 from "./web3.js";
 const address = "0x308cb6364ba407e9bf1b5A03886Da7B74dF96808";
-const abi = [
-  {
-    constant: true,
-    inputs: [Array],
-    name: "participants",
-    outputs: [Array],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "manager",
-    outputs: [Array],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [],
-    name: "pickWinner",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [],
-    name: "enterLottery",
-    outputs: [],
-    payable: true,
-    stateMutability: "payable",
-    type: "function"
-  },
-  {
-    inputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "constructor"
-  }
-];
+import lotteryA from "./build/LotteryA.json";
 
-export default new web3.eth.Contract(abi, address);
+export default new web3.eth.Contract(lotteryA.abi, address);
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
+import lotteryA from "../lottery";
+import web3 from "../web3";
 
 class Lottery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      manager: "",
+      participate_amount: "0.5",
+      message: ""
+    };
+  }
+  async componentDidMount() {
+    // get the public addres of the manager
+    const manager = await lotteryA.methods.manager().call();
+    console.log("Manager is: ", manager);
+    this.setState({ manager: manager });
+    console.log("Manager from state valiable is: ", this.state.manager);
+  }
+
   render() {
     return (
       <div>
         <h1>Total lottery pool is 1000 </h1>
         <form>
-          <input value=".5" />
+          <input placceholder="0.5" />
           <button type="submit">Participate</button>
         </form>
         <hr /> <br /> <hr />
-        <p>The manager of the lottery decentralaized app is </p>
+        <p>
+          The manager of the lottery decentralaized app is {this.state.manager}
+        </p>
         <button> Pick Winner </button>
       </div>
     );
